29b6d64f906677cef0945a9123d140d88ddd454d
theRealFallse <ababacarfall23@gmail.com>
2021-07-29 10:04:09 +0000

    gestion trace

--- src/Entity/Trace.php
+++ src/Entity/Trace.php
@@ -18,7 +18,7 @@ class Trace
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $details;
 
